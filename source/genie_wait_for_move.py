cd04803be3c4fe597aeae622fa366af4c237dcca
mattclarke <matt.clarke@stfc.ac.uk>
2016-01-20 10:21:39 +0000

    Checks alarm status, prints and returns the results

--- source/genie_wait_for_move.py
+++ source/genie_wait_for_move.py
@@ -9,14 +9,13 @@ Allows motors to complete their motion fully before proceeding."""
 # “alldone” indicates when any motion has completed and “moving” gives a count of moving motors.
 
 import time
-from datetime import timedelta, datetime
 
 
 class WaitForMoveController(object):
     def __init__(self, api, motion_pv):
         self._api = api
         self._motion_pv = motion_pv
-        self._polling_delay = 0.01
+        self._polling_delay = 0.02
         self._wait_succeeded = False
         self._missing_blocks = list()
 
@@ -41,9 +40,24 @@ class WaitForMoveController(object):
 
         If the motion does not start within the specified start_timeout then it will continue as if it did
         """
-        def prepped():
+        def check_blocks():
             return self._check_specific_motion(blocks)
-        self._do_wait(start_timeout, move_timeout, prepped)
+
+        self._do_wait(start_timeout, move_timeout, check_blocks)
+
+        # Check alarms
+        minor, major = self._check_alarms(blocks)
+        for i in major:
+            self._api.log_info_msg("WAITFOR_MOVE BLOCK %s IN MAJOR ALARM" % i)
+            print "Block %s is in alarm: MAJOR" % i
+        for i in minor:
+            self._api.log_info_msg("WAITFOR_MOVE BLOCK %s IN MINOR ALARM" % i)
+            print "Block %s is in alarm state: MINOR" % i
+        for i in self._missing_blocks:
+            self._api.log_info_msg("WAITFOR_MOVE BLOCK %s COULD NOT BE FOUND" % i)
+            print "Block %s could not be found" % i
+
+        return minor, major
 
     def _do_wait(self, start_timeout, move_timeout, check_for_move):
         # Pause very briefly to avoid any "double move" that may occur when multiple motors are moved
@@ -67,10 +81,10 @@ class WaitForMoveController(object):
 
     def _check_timeouts_valid(self, start_timeout, move_timeout):
         if start_timeout is not None and start_timeout <= 0:
-            self._api.log_info_msg("Start timeout cannot be less than zero - using default value")
+            self._api.log_info_msg("Start time out cannot be less than zero - using default value")
             start_timeout = 0
         if move_timeout is not None and move_timeout <= 0:
-            self._api.log_info_msg("Move timeout cannot be less than zero - using default value")
+            self._api.log_info_msg("Move time out cannot be less than zero - using default value")
             move_timeout = None
         return start_timeout, move_timeout
 
@@ -93,6 +107,7 @@ class WaitForMoveController(object):
     def _check_specific_motion(self, blocks):
         for b in blocks:
             if b in self._missing_blocks:
+                # Skip any missing blocks
                 continue
             name = self._api.correct_blockname(b)
             # DMOV = 0 when moving
@@ -108,3 +123,24 @@ class WaitForMoveController(object):
                 return True
 
         return False
+
+    def _check_alarms(self, blocks):
+        minor = list()
+        major = list()
+        for b in blocks:
+            if b in self._missing_blocks:
+                # Skip any missing blocks
+                continue
+            name = self._api.correct_blockname(b)
+            # Alarm states are: NO_ALARM, MINOR, MAJOR
+            try:
+                alarm_state = self._api.get_pv_value(name + ".SEVR", attempts=1)
+                if alarm_state == "MINOR":
+                    minor.append(b)
+                elif alarm_state == "MAJOR":
+                    major.append(b)
+            except:
+                # Could not get value
+                self._api.log_info_msg("WAITFOR_MOVE COULD NOT GET ALARM STATE FOR BLOCK: %s" % b)
+                print "\nCould not get alarm state for block %s" % b
+        return minor, major
