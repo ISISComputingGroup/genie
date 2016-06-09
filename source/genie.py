382742b0761c774ef919c8fcb9d52b75c3571d2d
Thomas Lohnert <thomas.lohnert@stfc.ac.uk>
2016-06-09 16:58:56 +0100

    removed spectraplot stuff

--- source/genie.py
+++ source/genie.py
@@ -1661,64 +1661,23 @@ def get_spectrum(spectrum, period=1, dist=False):
         _handle_exception(e)
 
 
-@usercommand
-@helparglist('spectrum[, period][, dist]')
 def plot_spectrum(spectrum, period=1, dist=False):
     """Get the specified spectrum from the DAE and plot it.
         
     Args:
         spectrum (int) : the spectrum number
-        period (int, optional) : the period. Default is 1
-        dist (bool, optional) : whether to get the spectrum as a distribution. Default is False
+        period (int, optional) : the period
+        dist (bool, optional) : whether to get the spectrum as a distribution
 
+    Returns:
+        GeniePlot : the plot object
     """
     __api.log_command(sys._getframe().f_code.co_name, locals())
-    __api.plots.remove_closed()
     try:
         graph = SpectraPlot(__api, spectrum, period, dist)
-        __api.plots.add_plot(graph)
+        return graph
     except Exception as e:
         _handle_exception(e)
-
-
-@usercommand
-@helparglist('spectrum[, period][, dist]')
-def add_spectrum(spectrum, period=1, dist=False, figure=None):
-    """Add a spectrum graph to an existing plot
-
-    Args:
-        spectrum (int) : the spectrum number
-        period (int, optional) : the period. Default is 1
-        dist (bool, optional) : whether to get the spectrum as a distribution. Default is False
-        figure (int, optional) : specifies which figure to plot the spectrum in. Default is last active plot
-
-    Examples:
-        Add Spectrum 2 to last active plot window
-        >>> add_spectrum(2)
-
-        Add Spectrum 1 to Figure 3
-        >>> add_spectrum(2, figure=3)
-
-        Add Spectrum 1 with period=2 to last active plot window as distribution
-        >>> add_spectrum(1, period=2, dist=True)
-
-        Add Spectrum 4 to Figure 1 as distribution
-        >>> add_spectrum(4, dist=True, figure=1)
-    """
-    __api.plots.remove_closed()
-    try:
-        if figure is None:
-            __api.plots.has_last_changed()
-            figure = __api.plots.get_last_plot()
-        else:
-            figure = __api.plots.get_plot(figure)
-            __api.plots.set_last_plot(figure)
-        figure.add_spectrum(spectrum, period, dist)
-    except Exception:
-        if figure is None:
-            print "Plotting failed: Create a plot first using plot_spectrum(<number>)"
-        else:
-            print "Plotting failed: Figure " + repr(figure) + " not found."
 
 
 @usercommand
