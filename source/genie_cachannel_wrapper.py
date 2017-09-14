2c614177ff6b3ed8fb64614fa377ba2980b853fb
Matt Clarke <matt.clarke@stfc.ac.uk>
2017-09-14 09:06:49 +0100

    Removed some unrelated technical debt

--- source/genie_cachannel_wrapper.py
+++ source/genie_cachannel_wrapper.py
@@ -1,28 +1,50 @@
+import six
 from CaChannel import ca, CaChannel, CaChannelException
 from threading import Event
 from utilities import waveform_to_string
-from channel_access_exceptions import UnableToConnectToPVException
+from channel_access_exceptions import UnableToConnectToPVException, InvalidEnumStringException, ReadAccessException, \
+    WriteAccessException
 
 TIMEOUT = 15         # default timeout for PV set/get
-EXIST_TIMEOUT = 3    # separate smaller timeout for pv_exists() and searchw() operations 
+EXIST_TIMEOUT = 3    # separate smaller timeout for pv_exists() and searchw() operations
 CACHE = dict()
 
 
 class CaChannelWrapper(object):
     @staticmethod
     def putCB(epics_args, user_args):
+        """
+        Callback used for setting PV values.
+
+        Args:
+            epics_args (tuple): Contains the results of the action.
+            user_args (tuple): Contains any extra arguments supplied to the call.
+
+        Returns:
+            None.
+        """
         user_args[0].set()
 
     @staticmethod
     def set_pv_value(name, value, wait=False, timeout=TIMEOUT):
-        """Set the PV to a value.
-           When getting a PV value this call should be used, unless there is a special requirement.
-        Args:
-            name - the PV name
-            value - the value to set
-            wait - wait for the value to be set before returning
         """
+        Set the PV to a value.
 
+        When getting a PV value this call should be used, unless there is a special requirement.
+
+        Args:
+            name (string): The PV name.
+            value: The value to set.
+            wait (bool, optional): Wait for the value to be set before returning.
+            timeout (optional): How long to wait for the PV to connect etc.
+
+        Returns:
+            None.
+
+        Raises:
+            UnableToConnectToPVException: If cannot connect to PV.
+            WriteAccessException: If write access is denied.
+        """
         if name in CACHE.keys() and CACHE[name].state() == ca.ch_state.cs_conn:
             chan = CACHE[name]
         else:
@@ -37,10 +59,10 @@ class CaChannelWrapper(object):
         chan.setTimeout(timeout)
 
         # Validate user input and format accordingly for mbbi/bi records
-        value = check_for_enum_value(name, value, chan)
+        value = CaChannelWrapper.check_for_enum_value(value, chan, name)
 
         if not chan.write_access():
-            raise Exception("Write access denied for PV %s" % name)
+            raise WriteAccessException(name)
         if wait:
             ftype = chan.field_type()
             ecount = chan.element_count()
@@ -59,7 +81,21 @@ class CaChannelWrapper(object):
 
     @staticmethod
     def get_pv_value(name, to_string=False, timeout=TIMEOUT):
-        """Get the current value of the PV"""
+        """
+        Get the current value of the PV.
+
+        Args:
+            name (name): The PV.
+            to_string (bool, optional): Whether to convert the value to a string.
+            timeout (optional): How long to wait for the PV to connect etc.
+
+        Returns:
+            The PV value.
+
+        Raises:
+            UnableToConnectToPVException: If cannot connect to PV.
+            ReadAccessException: If read access is denied.
+        """
         if name in CACHE.keys() and CACHE[name].state() == ca.ch_state.cs_conn:
             chan = CACHE[name]
         else:
@@ -73,7 +109,7 @@ class CaChannelWrapper(object):
             CACHE[name] = chan
         chan.setTimeout(timeout)
         if not chan.read_access():
-            raise Exception("Read access denied for PV %s" % name)
+            raise ReadAccessException(name)
         ftype = chan.field_type()
         if ca.dbr_type_is_ENUM(ftype) or ca.dbr_type_is_CHAR(ftype) or ca.dbr_type_is_STRING(ftype):
             to_string = True
@@ -94,7 +130,19 @@ class CaChannelWrapper(object):
 
     @staticmethod
     def pv_exists(name, timeout=EXIST_TIMEOUT):
-        """See if the PV exists"""
+        """
+        See if the PV exists.
+
+        Args:
+            name (string): The PV name.
+            timeout(optional): How long to wait for the PV to "appear".
+
+        Returns:
+            True if exists, otherwise False.
+
+        Raises:
+            UnableToConnectToPVException: If cannot connect to PV.
+        """
         if name in CACHE.keys() and CACHE[name].state() == ca.ch_state.cs_conn:
             return True
         else:
@@ -102,18 +150,29 @@ class CaChannelWrapper(object):
             chan.setTimeout(timeout)
             try:
                 CaChannelWrapper.connect_to_pv(chan)
-            except UnableToConnectToPVException as e:
-                print e.message
+            except UnableToConnectToPVException:
                 return False
             CACHE[name] = chan
             return True
 
     @staticmethod
     def connect_to_pv(ca_channel):
+        """
+        Connects to the PV.
+
+        Args:
+            ca_channel (CaChannel): The channel to connect to.
+
+        Returns:
+            None.
+
+        Raises:
+            UnableToConnectToPVException: If cannot connect to PV.
+        """
         event = Event()
         try:
             ca_channel.search_and_connect(None, CaChannelWrapper.putCB, event)
-        except CaChannelException as e:
+        except CaChannelException:
             raise UnableToConnectToPVException(ca_channel.pvname)
 
         interval = 0.1
@@ -127,26 +186,31 @@ class CaChannelWrapper(object):
             raise UnableToConnectToPVException(ca_channel.pvname)
 
     @staticmethod
-    def check_for_enum_value(value, chan):
+    def check_for_enum_value(value, chan, name):
         """
-        Test user input for MBBI/BI records, so that records can be set
-        by string values the user inputs.
+        Check for string input for MBBI/BI records and replace with the equivalent index value.
 
         Args:
-            value - The PV value.
-            chan - The channel access channel.
+            value: The PV value.
+            chan (CaChannel): The channel access channel.
+            name (string): The name of the channel.
 
         Returns:
-            Index value of enum, if the record is mbbi/bi. Otherwise, returns unchanged user input.
+            Index value of enum, if the record is mbbi/bi. Otherwise, returns unmodified value.
+
+        Raises:
+            InvalidEnumStringException: If the string supplied is not a valid enum value.
         """
         # If PV is MBBI/BI type, search list of enum values and iterate to find a match
-        if ca.dbr_type_is_ENUM(chan.field_type()):
+        # Use six to check string type as it works for Python 2 and 3.
+        if ca.dbr_type_is_ENUM(chan.field_type()) and isinstance(value, six.string_types):
             chan.array_get(ca.DBR_CTRL_ENUM)
             channel_properties = chan.getValue()
-            for index, enum_value in enumerate(channel_properties.get("pv_statestrings")):
-                if enum_value == value:
+            for index, enum_value in enumerate(channel_properties["pv_statestrings"]):
+                if enum_value.lower() == value.lower():
                     # Replace user input with enum index value
-                    value = index
-                    break
+                    return index
+            # If the string entered isn't valid then throw
+            raise InvalidEnumStringException(name, channel_properties["pv_statestrings"])
 
         return value
