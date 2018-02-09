e392bb4630fdf47da3c6848167d50e5bd06e1d2a
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-02-09 11:55:04 +0000

    Add test to catch wrong print statements

--- source/test_modules/test_scripts/valid.py
+++ source/test_modules/test_scripts/valid.py
@@ -1,8 +1,16 @@
 from __future__ import absolute_import
 from valid_to_import import HelloGenerator
 
+
+def test_both_styles_of_print_statement():
+    print("Hello")
+    print "Bonjour"
+    return True
+
+
 def valid():
     return True
+
 
 def check_import():
     HelloGenerator().greet()
