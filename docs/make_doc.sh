6d85f0cf09aa96a5e079035f0f7d7479be46ab6a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-03-05 19:41:22 +0000

    Build docs on linux

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 set -o errexit
 
-( cd ..; test -e genie_python || ln -s Lib/site-packages/genie_python genie_python )
-mkdir ../smslib
-mkdir ../smslib/sms
-touch ../smslib/__init__.py
-touch ../smslib/sms/__init__.py
+source ../package_builder/build-genie-python/venv/bin/activate
+python3 -m pip install -r requirements.txt
+make html
 
-env PYTHONPATH=.. make html
-
-( cd ..; test -h genie_python  && rm -f genie_python )
-rm -fr ../smslib
-
-rm -fr /isis/www/doxygen/genie_python/sphinx
-cp -r _build/html /isis/www/doxygen/genie_python/sphinx
+deactivate
