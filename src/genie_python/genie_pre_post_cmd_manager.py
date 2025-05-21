398b7129a24fb0bab4c4f20b80f9aac04fcd74e0
LilithCole <lilith.cole@stfc.ac.uk>
2025-05-21 16:32:11 +0100

    fix pre_post pyright, unfix genie advanced

--- src/genie_python/genie_pre_post_cmd_manager.py
+++ src/genie_python/genie_pre_post_cmd_manager.py
@@ -7,17 +7,38 @@ class PrePostCmdManager(object):
     A class to manager the precmd and postcmd commands such as used in begin, end, abort, resume,
      pause.
     """
+    def begin_precmd(self, **kwargs: Any) -> str | None:
+        return
 
-    def __init__(self) -> None:
-        self.begin_precmd: Callable[[Any], str | None] = lambda **pars: None
-        self.begin_postcmd: Callable[[Any], str | None] = lambda **pars: None
-        self.abort_precmd: Callable[[Any], str | None] = lambda **pars: None
-        self.abort_postcmd: Callable[[Any], str | None] = lambda **pars: None
-        self.end_precmd: Callable[[Any], str | None] = lambda **pars: None
-        self.end_postcmd: Callable[[Any], str | None] = lambda **pars: None
-        self.pause_precmd: Callable[[Any], str | None] = lambda **pars: None
-        self.pause_postcmd: Callable[[Any], str | None] = lambda **pars: None
-        self.resume_precmd: Callable[[Any], str | None] = lambda **pars: None
-        self.resume_postcmd: Callable[[Any], str | None] = lambda **pars: None
-        self.cset_precmd: Callable[[Any], str | bool | None] = lambda **pars: True
-        self.cset_postcmd: Callable[[Any], str | None] = lambda **pars: None
+    def begin_postcmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def abort_precmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def abort_postcmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def end_precmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def end_postcmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def pause_precmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def pause_postcmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def resume_precmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def resume_postcmd(self, **kwargs: Any) -> str | None:
+        return
+
+    def cset_precmd(self, **kwargs: Any) -> bool:
+        return True
+
+    def cset_postcmd(self, **kwargs: Any) -> str | None:
+        return
