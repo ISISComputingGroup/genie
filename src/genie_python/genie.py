3c0c3b56fc64faa11fdf2c8f2be1bd6d83631160
Jack Doughty <jackb.doughty@btinternet.com>
2025-07-14 10:31:31 +0100

    change func name

--- src/genie_python/genie.py
+++ src/genie_python/genie.py
@@ -2558,7 +2558,7 @@ def get_detector_table() -> str | None:
 
 @usercommand
 @log_command_and_handle_exception
-def get_autosave_freq() -> PVValue:
+def get_dae_autosave_freq() -> PVValue:
     """
     Gets the ICP autosave frequency (Frames).
     """
